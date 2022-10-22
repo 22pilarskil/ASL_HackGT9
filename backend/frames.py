2833d9fbba2eb462094825578ef5268b802cc2e5
Liam Pilarski <liampilarski@gmail.com>
2022-10-22 19:02:22 -0400

    Add scoring and time

--- backend/frames.py
+++ backend/frames.py
@@ -5,6 +5,7 @@ import pickle
 import argparse
 import serial
 import time
+import random
 
 
 # User defined constants
@@ -26,11 +27,23 @@ hands = mp_hands.Hands(
     min_tracking_confidence = 0.5,
     max_num_hands = 1
     )
+correct = True
+practice_letter = ''
+
+write = False
 
 camera = cv2.VideoCapture(0)
+start = time.time()
+score = 0
+
+def set_write(write_val):
+    global write, start, score
+    write = write_val
+    start = time.time()
+    score = 0
 
 def gen_frames():  
-
+    global correct, practice_letter, score
     start = time.time()
     letter, cur_letter, last_letter, chain = "", "", "", ""
     count = 0
@@ -54,11 +67,14 @@ def gen_frames():
                     for point in landmarks:
                         data_point += [point.x, point.y, point.z, point.visibility]
                     letter = str(knn.predict([data_point])[0])
-                    count = count + 1 if (cur_letter == letter and last_letter != letter) else 0
+                    count = count + 1 if (cur_letter == letter and (last_letter != letter or not write)) else 0
                     cur_letter = letter
                     if count > 5 and letter != "thumbs_down":
                         count = 0
                         if letter != "open": 
+                            if practice_letter == letter:
+                                correct = True
+                                score += 1
                             chain += letter
                             last_letter = letter
                         else: last_letter = ""
@@ -80,8 +96,25 @@ def gen_frames():
                         
             image = cv2.flip(image, 1)
             cv2.putText(image, letter, (50,350), fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (57, 255, 20), thickness=5)     
-            cv2.putText(image, chain, (50,450), fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (255, 57, 20), thickness=5)     
+            if write:
+                cv2.putText(image, chain, (50,450), fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (255, 57, 20), thickness=5)     
+            else:
+                cv2.putText(image, "Time elapsed: {} Score: {}".format(str(round(time.time() - start, 2)), score), 
+                        (50,600), fontFace = cv2.FONT_HERSHEY_COMPLEX, fontScale = 1.5, color = (57, 255, 20), thickness=5)    
 
             ret, buffer = cv2.imencode('.jpg', image)
             image = buffer.tobytes()
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
+
+
+def get_images():
+    global correct, practice_letter, score
+    while True:
+        letters = ['A','B','C','D']#,'E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y']
+        if correct:
+            practice_letter = random.choice(letters)
+            correct = False
+        image = cv2.imread('/Users/liampilarski/Desktop/ASL_HackGT9/frontend/build/signs/{}.jpg'.format(practice_letter))
+        ret, buffer = cv2.imencode('.jpg', image)
+        image = buffer.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
