dd112d890522fd0f80ff760508cf2e41c7f399ec
laurenmcp <lmcpeters3@gatech.edu>
2022-10-22 11:00:18 -0400

    push app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Practice from './pages/Practice.js';
+import Translate from './pages/Translate.js';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="/Home" element={<Home />}/>
+                <Route path="/Practice" element={<Practice />}/>
+                <Route path="/Translate" element={<Translate />}/>
+            </Route>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
